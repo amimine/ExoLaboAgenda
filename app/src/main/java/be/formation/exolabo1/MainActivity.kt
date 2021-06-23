5ffc8bc3df45810fe548b108f6cc11d87ade7181
bensennah amine <bensennahamine@gmail.com>
2021-06-23 17:04:12 +0200

    creation classes/ liaisons layout

--- app/src/main/java/be/formation/exolabo1/MainActivity.kt
+++ app/src/main/java/be/formation/exolabo1/MainActivity.kt
@@ -1,9 +1,10 @@
 package be.formation.exolabo1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -12,30 +13,39 @@ class MainActivity : AppCompatActivity() {
         private lateinit var btnAddEvent : Button
         private lateinit var tvMeteo : TextView
         private lateinit var rvEvents : RecyclerView
-
-    constructor(btnAddEvent: Button, tvMeteo: TextView, rvEvents: RecyclerView) {
-        this.btnAddEvent = btnAddEvent
-        this.tvMeteo = tvMeteo
-        this.rvEvents = rvEvents
-    }
+        private lateinit var btnPrevision : Button
+        private lateinit var btnEditEvent : Button
+        private lateinit var btnDeleteEvent : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Liaison avec le Layout
+
         btnAddEvent = findViewById(R.id.btn_add_event)
         tvMeteo = findViewById(R.id.tv_meteo)
         rvEvents = findViewById(R.id.rv_events)
+        btnPrevision = findViewById(R.id.btn_prevision)
+        btnEditEvent = findViewById(R.id.btn_edit_event)
+        btnDeleteEvent = findViewById(R.id.btn_delete_event)
+
+        btnAddEvent.setOnClickListener(View.OnClickListener {it
+            (R.id.cv_agenda)
 
 
+        })
+        btnEditEvent.setOnClickListener(View.OnClickListener {it
+            (R.id.rv_events)
+        })
 
+        btnDeleteEvent.setOnClickListener(View.OnClickListener {it
+            (R.id.rv_events)
+        })
+
+        btnPrevision.setOnClickListener((View.OnClickListener { it
+            (R.id.tv_prevision)
+        }))
     }
 
-    fun addEvent() {
-
-    }
-
-
-
-}
+    };
